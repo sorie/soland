71a17b967cd0368555d7066b8226152feb0c0019
master <enne3939@gmail.com>
2021-07-09 14:06:06 +0900

    add pause(), select video/audio list

--- src/views/VideoChat.vue
+++ src/views/VideoChat.vue
@@ -11,9 +11,15 @@
 			<button @click="videoStart">
 				start
 			</button>
+			<button @click="videoPause">
+				pause
+			</button>
 		</div>
 		<div class="cont">
-			<button @click="getDevices">get devices start</button>
+			<button @click="getDevices('videoinput')">get camera list</button>
+			<select name="" id="availableCameras"></select>
+			<button @click="getDevices('audioinput')">get audio list</button>
+			<select name="" id="availableAudios"></select>
 		</div>
 	</section>
 </template>
@@ -30,19 +36,54 @@ export default {
 				const stream = await navigator.mediaDevices.getUserMedia(constraints);
 				const videoElement = document.getElementById('getVideo');
 				videoElement.srcObject = stream;
+				let videoTracks = await stream.getVideoTracks();
+        console.info(`media Devices label : ${videoTracks[0].label}`);
 			}
 			catch(error){
 				console.error(error);
 			}
 		},
-		async getDevices() {
+		videoPause() {
+			const videoElement = document.getElementById('getVideo');
+			if(videoElement.paused) videoElement.play();
+			else videoElement.pause()
+		},
+		async getDevices(type) {
+			const devices = await this.getConnectedDevices(type);
+			await this.updateList(devices, type);
+
+			navigator.mediaDevices.addEventListener('devicechange', event => {
+				const newCameraList = this.getConnectedDevices('video');
+				const newAudioList = this.getConnectedDevices('audio');
+				this.updateList(newCameraList, 'video');
+				this.updateList(newAudioList, 'audio');
+			})
+		},
+		async getConnectedDevices(type) {
 			try {
 				const devices = await navigator.mediaDevices.enumerateDevices();
 				console.info(devices);
+				return devices.filter(device => device.kind === type);
 			}
 			catch(error){
 				console.error(error);
 			}
+		},
+		updateList(devices, type){
+			let listElement;
+			if(type === 'videoinput' || type === 'video'){
+				listElement = document.querySelector('select#availableCameras');
+			}
+			else if(type === 'audioinput' || type === 'audio'){
+				listElement = document.querySelector('select#availableAudios');
+			}
+			listElement.innerHTML = '';
+			devices.map(device => {
+        const deviceOption = document.createElement('option');
+        deviceOption.label = device.label;
+        deviceOption.value = device.deviceId;
+				listElement.add(deviceOption)
+    	});
 		}
 	}
 }
