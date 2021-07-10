6e89c39ca52fb57ef589c43a1eb25b1b9e921950
sori <enne3939@gmail.com>
2021-07-10 19:20:48 +0900

    [Add availables, beforeCreate()] check mobile or pc

--- src/views/VideoChat.vue
+++ src/views/VideoChat.vue
@@ -24,8 +24,73 @@
 	</section>
 </template>
 <script>
+import config from '../config'
+
 export default {
 	name: 'VideoChat',
+	data() {
+		return {
+			bSupportBrowser : true,
+			ismobiletype : true,
+			devicetype : ''
+		}
+	},
+	beforeCreate() {
+		let userAgent = navigator.userAgent;
+
+		if(userAgent.match(/Mobile|Windows Phone|Lumia|Android|webOS|iPhone|iPod|Blackberry|PlayBook|BB10|Opera Mini|\bCrMo\/|Opera Mobi/i) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) ){
+			if(userAgent.match(/iPhone|iPod|iPad/i) || navigator.platform === 'MacIntel'){
+					this.devicetype = 'iOS';
+			}
+			else{
+					this.devicetype = 'Android';
+			}
+
+			if(this.devicetype==='iphone'){
+					if(navigator.userAgent.match('CriOS') || navigator.userAgent.match('FxiOS')) {
+							this.bSupportBrowser = false;
+					}
+					else {
+							// iphone은 safari만 지원.
+							let safariversion = parseFloat(userAgent.substr(userAgent.indexOf("Version")).split(' ')[0].split('/')[1]);
+							if(userAgent.indexOf('NAVER') !== -1) {
+								this.bSupportBrowser = false;
+							}else if(userAgent.indexOf('KAKAOTALK') !== -1) {
+								this.bSupportBrowser = false;
+							}else if(safariversion < 11.0){
+								this.bSupportBrowser = false;
+							}
+							else{
+								if(navigator.mediaDevices){
+									this.bSupportBrowser = true;
+								}
+								else{
+									this.bSupportBrowser = false;
+								}
+							}
+					}
+			}
+			else{
+					if(userAgent.indexOf('Chrome') !== -1) {
+							// android는 chrome만 지원.
+							if(userAgent.indexOf('NAVER') !== -1) {
+									this.bSupportBrowser = false;
+							}else if(userAgent.indexOf('KAKAOTALK') !== -1) {
+									this.bSupportBrowser = false;
+							}else{
+									this.bSupportBrowser = true;
+							}
+					}
+					else{
+							this.bSupportBrowser = false;
+					}
+			}
+		}
+		else{
+				this.ismobiletype = false;
+				this.devicetype = 'pc';
+		}
+	},
 	methods: {
 		async videoStart() {
 			try {
