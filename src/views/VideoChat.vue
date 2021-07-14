554df6d98a86e062e63f9673ee1d8b9c2497c779
master <enne3939@gmail.com>
2021-07-14 18:02:01 +0900

    [add]add video tag, adding connect peer function

--- src/views/VideoChat.vue
+++ src/views/VideoChat.vue
@@ -1,9 +1,17 @@
 <template>
 	<section class="container videoChat">
-		<h1>webRTC transparent</h1>
+		<h1>webRTC for video chat test</h1>
 		<div class="cont">
+			<h2>1. get video</h2>
 			<video 
 				id="getVideo" 
+				class="getVideo"
+				autoplay 
+				playsinline 
+				controls
+			/>
+			<video 
+				id="getVideoTwo" 
 				class="getVideo"
 				autoplay 
 				playsinline 
@@ -19,6 +27,7 @@
 			</div>
 		</div>
 		<div class="cont">
+			<h2>2. get device information</h2>
 			<div class="selectWrap">
 				<select name="" id="availableCameras"></select>
 				<button @click="getDevices('videoinput')">get camera list</button>
@@ -26,6 +35,14 @@
 			<div class="selectWrap">
 				<select name="" id="availableAudios"></select>
 				<button @click="getDevices('audioinput')">get audio list</button>
+			</div>
+		</div>
+		<div>
+			<h2>3. connection webRtc</h2>
+			<div class="btnWrap">
+				<button @click="videoStart">start</button>
+				<button id="btn_connect" @click="connectPeer">connect</button>
+				<button id="btn_hangUp" @click="hangUp">hang up</button>
 			</div>
 		</div>
 	</section>
@@ -41,6 +58,9 @@ export default {
 			ismobiletype : true,
 			devicetype : '',
 			adapter: adapter,
+			localStream: null,
+			pc1: null,
+			pc2: null,
 		}
 	},
 	beforeMount() {
@@ -112,7 +132,7 @@ export default {
 			if(window.navigator.userAgent.toLowerCase().indexOf("edg") > -1){
 					// wss websocket not supported
 					this.appConfig.browsertype = 'edg';
-					bSupportBrowser = false;
+					this.bSupportBrowser = false;
 			}
 		}	
 
@@ -121,6 +141,10 @@ export default {
 		}
 	},
 	mounted() {
+		const btn_connect = document.getElementById('btn_connect');
+		const btn_hangup = document.getElementById('btn_hangUp');
+		btn_connect.disabled = true;
+		btn_hangup.disabled = true;
 		navigator.mediaDevices.addEventListener('devicechange', event => {
 			const newCameraList = this.getConnectedDevices('video');
 			const newAudioList = this.getConnectedDevices('audio');
@@ -133,13 +157,18 @@ export default {
 			try {
 				const constraints = {
 					'video': true,
-					'audio': false
+					'audio': true
 				}
 				const stream = await navigator.mediaDevices.getUserMedia(constraints);
 				const videoElement = document.getElementById('getVideo');
 				videoElement.srcObject = stream;
+				this.localStream = stream;
 				let videoTracks = await stream.getVideoTracks();
         console.info(`media Devices label : ${videoTracks[0].label}`);
+				const btn_connect = document.getElementById('btn_connect');
+				const btn_hangup = document.getElementById('btn_hangUp');
+				btn_connect.disabled = false;
+				btn_hangup.disabled = false;
 			}
 			catch(error){
 				console.error(error);
@@ -179,22 +208,78 @@ export default {
         deviceOption.value = device.deviceId;
 				listElement.add(deviceOption)
     	});
+		},
+		connectPeer() {
+			let startTime = window.performance.now();
+			const videoTracks = this.localStream.getVideoTracks();
+			const audioTracks = this.localStream.getAudioTracks();
+			if(videoTracks.length > 0) {
+				console.info(`Using video device: ${videoTracks[0].label}`)
+			}
+			if(audioTracks.length > 0) {
+				console.info(`Using audio device: ${audioTracks[0].label}`)
+			}
+			const configuration = {};
+			this.pc1 = new RTCPeerConnection(configuration);
+			this.pc1.addEventListener('icecandidate', e => this.onIceCadidate(pc1, e));
+
+		},
+		hangUp() {
+			console.log('Ending call');
+			pc1.close();
+			pc2.close();
+			pc1 = null;
+			pc2 = null;
+			hangupButton.disabled = true;
+			callButton.disabled = false;
+		},
+		async onIceCandidate(pc, event) {
+			try {
+				await (this.getOtherPc(pc).addIceCandidate(event.candidate));
+				onAddIceCandidateSuccess(pc);
+			} catch (e) {
+				onAddIceCandidateError(pc, e);
+			}
+			console.log(`${this.getName(pc)} ICE candidate:\n${event.candidate ? event.candidate.candidate : '(null)'}`);
+		},
+		getName(pc) {
+			return (pc === pc1) ? 'pc1' : 'pc2';
+		},
+		getOtherPc(pc) {
+			return (pc === pc1) ? pc2 : pc1;
 		}
 	}
 }
 </script>
 <style lang="scss" scoped>
+.videoChat {
+	max-width: 1000px;
+	width: 80%;
+	margin: 0 auto;
+}
 video.getVideo {
-	width: 100%;
+  --width: 45%;
+  width: var(--width);
+  height: calc(var(--width) * 0.75);
+  margin: 0 0 20px 0;
+  vertical-align: top;
+}
+video#getVideo {
+  margin: 0 20px 20px 0;
 }
 .cont {
 	margin-bottom: 3em;
 }
 button {
 	border: none;
-	background: rgba(255,255,255,.1);
+	background: rgba(255,255,255,.2);
 	padding: 1em 1.5em;
 	color: #fff;
+	cursor: pointer;
+}
+button:disabled {
+	background: rgba(255,255,255,.08);
+	cursor: not-allowed;
 }
 .btnWrap>button {
 	margin-right: 1em;
