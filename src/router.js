113b46660438a344b0b1a1786331bd01bfd00cfa
sori <enne3939@gmail.com>
2021-07-24 20:14:53 +0900

    [modify] git pages

--- src/router.js
+++ src/router.js
@@ -11,6 +11,10 @@ export default new Router({
   mode: 'history',
   routes: [
     {
+      path: '/soland',
+      redirect: '/List'
+    },
+    {
       path: '/',
       redirect: '/List'
     },
