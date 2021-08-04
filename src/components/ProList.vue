0f7f5e491cf35561daa8e4d1cd92d81bddc7d542
master <enne3939@gmail.com>
2021-08-04 16:15:27 +0900

    [modify css] for responsive web

--- src/components/ProList.vue
+++ src/components/ProList.vue
@@ -5,25 +5,28 @@
     class="container projects" 
     :style="[mainStyle]"
   >
-    <div class="mainTable">
-      <div class="tableWrap">
-        <table>
-          <thead>
-            <tr>
-              <td v-for="(item, idx) in project.titlename" :key="`item-newmain-${idx}`">{{project.titlename[idx]}}</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item, idx) in projectListVal.items" :key="`item-newmain-${idx}`">
-              <td>{{item.name}}</td>
-              <td v-html=item.date>{{item.date}}</td>
-              <td>{{item.workspace}}</td>
-              <td>{{item.language}}</td>
-              <td v-html=item.role></td>
-              <td>{{item.type}}</td>
-            </tr>
-          </tbody>
-        </table>
+    <div class="mainWrap">
+      <h1>My project list</h1>
+      <div class="mainTable">
+        <div class="tableWrap">
+          <table>
+            <thead>
+              <tr>
+                <td v-for="(item, idx) in project.titlename" :key="`item-newmain-${idx}`">{{project.titlename[idx]}}</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, idx) in projectListVal.items" :key="`item-newmain-${idx}`">
+                <td>{{item.name}}</td>
+                <td v-html=item.date>{{item.date}}</td>
+                <td>{{item.workspace}}</td>
+                <td>{{item.language}}</td>
+                <td v-html=item.role></td>
+                <td>{{item.type}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -65,26 +68,39 @@ export default {
 </script>
 // scoped 속성을 가지고있을 때, CSS는 현재 컴포넌트의 엘리먼트에만 적용
 <style lang="scss" scoped>
+body {
+  overflow-x: hidden;
+}
 .projects {
   height: 100%;
   background-size: cover;
   background-repeat: no-repeat;
+  >.mainWrap {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 1em;
+  }
 }
 .mainTable {
-  max-width: 1000px;
-  margin: 7rem auto;
-  overflow-x:hidden;
+  overflow-x: hidden;
+  overflow-y: hidden;
   .tableWrap{
     width: calc(100% + 20px);
-    height: 937px;
+    height: 60vh;
+    overflow-x: scroll;
     overflow-y: scroll;
+    webkit-overflow-scrolling: touch;
   }
   table {
+    min-width: 50rem;
     background:rgba(255,255,255,0.1);
     border-collapse: collapse;
     thead {
-      border-top: 1px solid white;
-      border-bottom: 2px solid white;
+      position: sticky;
+      top: 0px;
+      background-color: #003d33;
+      color: #ffffff;
+      font-size: 0.9rem;
       text-align: center;
       line-height: 3em;
       td:nth-child(1) {
