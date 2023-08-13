418e34b589f0a15241a113d0caae1cb2355c8cf0
sori_jeong <enne3939@gmail.com>
2023-08-13 12:19:33 +0900

    emplify build fix

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ ES6Promise.polyfill()
 Vue.config.productionTip = false
 Vue.use(srEngine);
 Vue.prototype.appConfig = config
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 new Vue({
   router,
