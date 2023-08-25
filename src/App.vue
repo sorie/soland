95f479639681d706307cedefac62281f0cd69a1f
sori_jeong <enne3939@gmail.com>
2023-08-25 12:11:34 +0900

    [오류 처리중] getUserMedia 호출 오류처리중 (버전 마이그레이션 중 생긴 오류)

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
       class="menu">
         <router-link to="/cont_list">List</router-link>
         <router-link to="/cont_examples">Components</router-link>
-        <router-link to="/cont_videoChat">webRTC</router-link>
+        <router-link to="/cont_videoChat" @click="notworking()">webRTC</router-link>
       </ul>
       <span 
         class="main__header-hide" 
@@ -57,6 +57,9 @@ export default {
         console.log("error")
       }
       
+    },
+    notworking() {
+      return alert('오류 검사 중입니다.');
     }
   }
 }
